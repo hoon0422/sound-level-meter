8ce3bed0145815eccb572e8418fc91d7fa84b424
Younghoon Jeong <yjeong2@whova.com>
2026-04-12 22:09:47 -0400

    Fix constants

--- audio/engine/constants.ts
+++ audio/engine/constants.ts
@@ -1,11 +1,7 @@
 import { AudioEngineConfig } from "./types";
 
 export const DEFAULT_AUDIO_ENGINE_CONFIG: AudioEngineConfig = {
-<<<<<<< HEAD
   sampleRate: 48000,
-=======
-  sampleRate: 44100,
->>>>>>> 51a2880 (feat: restructure audio processing with new microphone and spectrum analysis controllers, integrating zustand for state management)
   fftSize: 1024,
   smoothingTimeConstant: 0.3,
   minDecibels: -90,
