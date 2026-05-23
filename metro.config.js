e76f04dc8e7e5ea9631286e9ffe7f9b521c591e4
Younghoon Jeong <yjeong2@whova.com>
2026-05-23 17:09:12 -0400

    포맷 체크 통과

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer.babelTransformerPath = require.resolve(
-  "react-native-svg-transformer/expo"
-);
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer/expo');
 
-config.resolver.assetExts = config.resolver.assetExts.filter(
-  (ext) => ext !== "svg"
-);
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
 
-config.resolver.sourceExts.push("svg");
+config.resolver.sourceExts.push('svg');
 
 module.exports = config;
