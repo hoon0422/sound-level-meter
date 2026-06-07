e9df7f8c755dbcb7109a894d446e4a282cc876aa
Younghoon Jeong <yjeong2@whova.com>
2026-06-07 00:44:45 -0400

    Fix SVG icon transformer config

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,15 @@ const config = getSentryExpoConfig(__dirname, {
   annotateReactComponents: true,
 });
 
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...new Set([...config.resolver.sourceExts, 'svg'])],
+};
+
 module.exports = config;
